de9decabe1ffc19044afaf11adc73f7abe8ae83d
NuclMe <saltansergei@gmail.com>
2021-07-21 11:25:14 +0300

    finished hw

--- SecondLesson/HW/eight.js
+++ SecondLesson/HW/eight.js
@@ -3,4 +3,39 @@
 // 2. При чтении значения fullName оно должно получаться из объединения firstName и lastName
 // 3. При задании значения свойства dateOfBirth должно так же устанавливаться свойство age в зависимости от разницы года рождения и текущего года
 
-const 
+// Все работает, кроме проверок, я их закоментил, а то они срабатывают по дефолту и ничего не проверяют
+const human = {
+    firstName: '',
+    lastName: '',
+    age: Number,
+    
+    get fullName(){
+        return `${human.firstName} ${human.lastName}`
+    },
+    set fullName(value){
+        // if(value != ''){
+        //     console.log('value,should be a string')
+        // }
+        // if(value = ' '){
+        //     console.log('value,shouldnt be empty')
+        // }
+        const parts = value.split(' ')
+        this.firstName = parts[0]
+        this.lastName = parts[1]
+    },
+    get birthDate(){
+        return `${human.age}`
+    },
+    set birthDate(value){
+        // if(value != Number){
+        //     console.log('value,should be only number')
+        // }
+        this.age = value;
+        const research = new Date().getFullYear() - this.age;
+        this.age = research;   
+    }
+     
+};
+human.fullName = 'Serg Brin';
+human.birthDate = 2001
+console.log(human)
