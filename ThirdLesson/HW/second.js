f9c16fe5e197d04a9e3d5f206cefd66fcb06a7c4
NuclMe <you@example.com>
2021-07-21 23:31:18 +0300

    added half of hw 3

--- ThirdLesson/HW/second.js
+++ ThirdLesson/HW/second.js
@@ -12,16 +12,40 @@
 // calculate(2, 10); // { value: 1024, fromCache: false}
 // calculate(2, 10); // { value: 1024, fromCache: true}
 
-function cache(a,b) {
-    const res = Math.pow(a,b)
+function cache() {
     let arr = [];
-    const pushto = arr.push(res)
-    console.log(arr) 
-   
-    
-    
+    return function (a, b) {
+        let isCached = false;
+        let value = 0;
+        let cacheIndex = 0;
+
+        arr.forEach(function (first, second) {
+            if (first.a === a && first.b === b) {
+                isCached = true
+                cacheIndex = second
+            }
+        });
+        if (isCached) {
+            value = arr[cacheIndex].value  
+        } else {
+            value = Math.pow(a, b)
+            arr.push({
+                a,
+                b,
+                value
+            })
+        }
+        return { value, isCached }
+    }
 }
-cache(3, 5)
+const calculate = cache()
+
+
+// console.log(calculate(3, 3)) // { value: 27, fromCache: false}
+// console.log(calculate(2, 10)) // { value: 1024, fromCache: false}
+console.log(calculate(2, 10)) // { value: 1024, fromCache: true}
+console.log(calculate(2, 10)) // { value: 1024, fromCache: true}
+
 // 0) записывать аргумент и их результат 
 // 1) проверять есть ли такие аргументы или нет
 // 2) если есть, то выкидывать результат сразу 
