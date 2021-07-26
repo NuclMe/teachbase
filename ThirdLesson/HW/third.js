754ee548eb27810c217c4445f1d6c8b1486fe714
NuclMe <saltansergei@gmail.com>
2021-07-26 12:09:51 +0300

    added some logic to 3 task 3 lesson

--- ThirdLesson/HW/third.js
+++ ThirdLesson/HW/third.js
@@ -13,66 +13,75 @@
 // План
 // 0) Генератор случайного шестизначного id  ЕСТЬ
 // 1) метод создания концерта ЕСТЬ 
-// 2) метод создания цены билета на вышеупомянутый концерт (может их как-то обьеденить???) ОБЬЕДЕНИЛ
-// 3) метод покупки билета с добавление его цены в кассу 
-// 4) метод, который после покупки вернет тебе шестизначный id билета и пометить, что этот билет уже продан  
+// 2) метод создания цены билета на вышеупомянутый концерт (может их как-то обьеденить???) ОБЪЕДИНИЛ
+// 3) метод покупки билета с добавление его цены в кассу  
+// 4) метод, который после покупки вернет тебе шестизначный id билета и пометить, что этот билет уже продан  ЕСТЬ
 // 5) метод, который будет возвращать билет по вводу в него id билета, если он помечен кака продан, то из кассы вычитаем сумму 
 
 
 function TicketWindow() {
     const events = [];
-    const cash = []
+    // const cash = []
     
-    // создание уникального id "НИЧЕГО НЕ ВОЗВРАЩАЕТ"
+    // метод создания уникального id
     this.generateId = () => (new Date()).getTime();
     
-    // создание ивента 
+    // метод создания ивента 
     this.createEvent = (eventName, eventPrice) => {
-        // создал переменную в которую засунул цену и название концерта 
         const eventId = this.generateId();
         const eventData = { 
             eventName,
             eventPrice,
             eventId,
             soldTickets: [],
+            eventCash: [],
         };
         events.push(eventData);
         return eventData;        
     }
 
+// метод покупки билета         
     this.buyTicket = (eventName,eventPrice) => {
         if(events.length){
            for(let i = 0; i < events.length; i++){
             if(events[i].eventName === eventName ){
                 const ticketId = this.generateId();
                 const ticketPrice = eventPrice;
-                console.log(ticketId,ticketPrice)
-                events[i].soldTickets.push({ticketId:ticketId,ticketPrice: ticketPrice});
-                // events[i].cash.push(ticketPrice)
+                // пушу цену в массив eventCash
+                events[i].eventCash.push(ticketPrice)
+                // тут стараюсь его перебрать и добавить цену, но бьет ошибкуф
+                // let sum = 0;
+                // for(let i = 0; i < eventCash.length; i++){
+                //     sum += eventCash[i]
+                // }
+                events[i].soldTickets.push(ticketId);
                 return ticketId;
             }
            }
         }
     }
-
-    this.returnTicket = ticketId => {
+    
+// метод возврата билета
+    this.returnTicket = (ticketId,eventPrice) => {
         for(const e of events){
             for(const tid of e[soldTickets]) {
                 if(ticketId===tid){
-                    return 'already sold'
-                    // cash.
+                    // пытаюсь удалить первый найденый совпадающий по id елемент. 
+                    events.splice[0,1]
+                    // перебираю массив
+                    for(i=0; eventCash.length; i++){}
+                    // отнимаю от первого(и единственного) елемента цену ивента
+                    let sum = eventCash[i] - eventPrice
+                    return eventCash;
                 }
             }
         }
-      
     }
-    
 }
 
 const testWindow = new TicketWindow();
 const testEvent = testWindow.createEvent('rave',900);
-const testEvent2 = testWindow.createEvent('rap event',445);
-const testTicket = testWindow.buyTicket('rave',800);
-const testTicket2 = testWindow.buyTicket('rap event',445);
+const testTicket = testWindow.buyTicket('rave',900);
+const testTicketReturn = testWindow.returnTicket(1627283998658);
 
 console.log(testTicket,testEvent);
