2bd11274e1837a13c610253c688108dbd6fd7031
NuclMe <saltansergei@gmail.com>
2021-07-26 14:27:41 +0300

    commit to fix

--- ThirdLesson/HW/third.js
+++ ThirdLesson/HW/third.js
@@ -34,7 +34,7 @@ function TicketWindow() {
             eventPrice,
             eventId,
             soldTickets: [],
-            eventCash: [],
+            eventCash: 0,
         };
         events.push(eventData);
         return eventData;        
@@ -47,15 +47,12 @@ function TicketWindow() {
             if(events[i].eventName === eventName ){
                 const ticketId = this.generateId();
                 const ticketPrice = eventPrice;
-                // пушу цену в массив eventCash
-                events[i].eventCash.push(ticketPrice)
-                // тут стараюсь его перебрать и добавить цену, но бьет ошибкуф
-                // let sum = 0;
-                // for(let i = 0; i < eventCash.length; i++){
-                //     sum += eventCash[i]
-                // }
+                /*  записываю цену билета в переменную. ПРИДУМАТЬ как передать эти значения в eventData  в ключ eventCash.
+                 пробовал через return,но ничего не получилось. Наверно надо и дальше в эту сторону "копать". Был бы eventCash массивом, но создавать ради 1го числа массив */ 
+                const AllCash = events[i].eventCash + ticketPrice;
+                console.log(AllCash)         
                 events[i].soldTickets.push(ticketId);
-                return ticketId;
+                return 999;
             }
            }
         }
@@ -64,15 +61,12 @@ function TicketWindow() {
 // метод возврата билета
     this.returnTicket = (ticketId,eventPrice) => {
         for(const e of events){
-            for(const tid of e[soldTickets]) {
+            for(const tid of e['soldTickets']) {
                 if(ticketId===tid){
                     // пытаюсь удалить первый найденый совпадающий по id елемент. 
-                    events.splice[0,1]
-                    // перебираю массив
-                    for(i=0; eventCash.length; i++){}
-                    // отнимаю от первого(и единственного) елемента цену ивента
-                    let sum = eventCash[i] - eventPrice
-                    return eventCash;
+                    events.splice[0,1]                     
+                    let sum = eventCash - eventPrice
+                    return sum;
                 }
             }
         }
@@ -82,6 +76,6 @@ function TicketWindow() {
 const testWindow = new TicketWindow();
 const testEvent = testWindow.createEvent('rave',900);
 const testTicket = testWindow.buyTicket('rave',900);
-const testTicketReturn = testWindow.returnTicket(1627283998658);
+const testTicketReturn = testWindow.returnTicket(1627295692359);
 
 console.log(testTicket,testEvent);
